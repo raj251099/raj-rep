c35f762e6fdda6558da8ca8fc20e1816e7a87144
raj251099 <102846756+raj251099@users.noreply.github.com>
2022-04-02 12:29:45 +0530

    7th one

---
+++ demo.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+var fname =prompt("enter your first name :");
+var lname =prompt("enter your last name :");
+console.log("your full name is",fname,lname);
+console.log(" its working");
